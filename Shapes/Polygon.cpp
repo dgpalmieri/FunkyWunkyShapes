d9226ed5ff190687b06e04c1b4eb01b9743e0445
Dylan Palmieri <dgpalmieri@gmail.com>
2019-03-25 14:43:33 -0800

    Add public body functions to Polygon

--- Shapes/Polygon.cpp
+++ Shapes/Polygon.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Polygon.hpp"
+#include <iostream>
+#include <fstream>
+
+Polygon::Polygon(int numOfSides, double sideLength)
+    : _numOfSides(numOfSides), _sideLength(sideLength)
+    {}
+
+void Polygon::intoPS() {
+
+}
+
+double Polygon::getWidth() {
+    return 0;
+}
+
+double Polygon::getHeight() {
+    return 0;
+}
--- Shapes/Polygon.hpp
+++ Shapes/Polygon.hpp
@@ -5,10 +5,18 @@
 #include "Shape.h"
 
 #ifndef FUNKYWUNKYSHAPES_POLYGON_HPP
-#define FUNKYWUNKYSHAPES_POLYGON_HPP 
+#define FUNKYWUNKYSHAPES_POLYGON_HPP
 
 class Polygon: public Shape {
+public:
+    Polygon(int numOfSides, double sideLength);
+    void intoPS() override;
+    double getWidth() override;
+    double getHeight() override;
 
+private:
+    int _numOfSides;
+    double _sideLength;
 };
 
 
