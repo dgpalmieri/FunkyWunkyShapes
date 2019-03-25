b0153a286fbd25050f351b869151ed46b4676e45
Dylan Palmieri <dgpalmieri@gmail.com>
2019-03-25 15:27:50 -0800

    Add width and height functions

--- Shapes/Polygon.cpp
+++ Shapes/Polygon.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <fstream>
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 Polygon::Polygon(int numOfSides, double sideLength)
     : _numOfSides(numOfSides), _sideLength(sideLength)
     {}
@@ -15,9 +18,36 @@ void Polygon::intoPS() {
 }
 
 double Polygon::getWidth() {
-    return 0;
+    double width = 0;
+
+    if (_numOfSides % 2 == 1){
+        width = (M_E * sin(M_PI * (_numOfSides - 1) / 2 * _numOfSides))/
+                (sin(M_PI / _numOfSides));
+    }
+    else if (_numOfSides % 4 == 0){ //(e cos(π/n))/(sin(π/n))
+        width = (M_E * cos(M_PI / _numOfSides)) /
+                (sin(M_PI / _numOfSides));
+    }
+    else { // e/(sin(π/n))
+        width = M_E /
+                (sin(M_PI / _numOfSides));
+    }
+
+    return width;
 }
 
 double Polygon::getHeight() {
-    return 0;
+    int height = 0;
+
+    if (_numOfSides % 2 == 1){
+
+    }
+    else if (_numOfSides % 4 == 0){
+
+    }
+    else {
+
+    }
+
+    return height;
 }
