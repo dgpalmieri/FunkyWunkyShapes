7f1b42e26a3e4b45a442077575061e8af0fcb5f8
Dylan Palmieri <dgpalmieri@gmail.com>
2019-03-25 14:22:09 -0800

    Add new Polygon class

---
+++ Shapes/Polygon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dylan Palmieri on 3/25/2019.
+//
+
+#include "Polygon.hpp"
---
+++ Shapes/Polygon.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Dylan Palmieri on 3/25/2019.
+//
+
+#include "Shape.h"
+
+#ifndef FUNKYWUNKYSHAPES_POLYGON_HPP
+#define FUNKYWUNKYSHAPES_POLYGON_HPP 
+
+class Polygon: public Shape {
+
+};
+
+
+#endif //FUNKYWUNKYSHAPES_POLYGON_HPP
