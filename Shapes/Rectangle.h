a758f83646adcf89149ba6eb33e7f6269e9b74d1
arparker5 <arparker5@alaska.edu>
2019-03-26 13:16:40 -0800

    Removing Cmake from .gitignore, fixed bug in rectangle.h

---
+++ Shapes/Rectangle.h
@@ -0,0 +1,22 @@
+//
+// Created by Joseph Byam on 3/26/2019.
+//
+
+#ifndef FUNKYWUNKYSHAPES_RECTANGLE_H
+#define FUNKYWUNKYSHAPES_RECTANGLE_H
+
+#include "Shape.h"
+
+class Rectangle : public Shape{
+public:
+    explicit Rectangle(const double & width, const double & height);
+    void intoPS() override;
+    int getWidth() override;
+    int getHeight() override;
+private:
+    double _width;
+    double _height;
+};
+
+
+#endif //FUNKYWUNKYSHAPES_RECTANGLE_H
