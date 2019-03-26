42e27f0590ed8559c4f9c84a47b2064f01044c2e
Joseph Byam <jmbyam@alaska.edu>
2019-03-26 10:22:48 -0800

    I made the stuff for the Rectangle. It might not be done, I left comments.

---
+++ Shapes/Rectangle.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Joseph Byam on 3/26/2019.
+//
+
+#include "Rectangle.h"
+#include <cmath>
+#include <fstream>
+
+Rectangle::Rectangle(const double & width, const double & height)
+    :_width(std::abs(width)), _height(std::abs(height)) {}
+
+//So this makes a rectangle and I know that the width and height are in the right places
+//but I'm not sure where its supposed to start from. I just picked 0 because it seemed good.
+//If it needs to be somewhere else just change all the zeros to the starting point.
+void Rectangle::intoPS() {
+    std::ofstream myfile;
+    myfile.open("PostScriptTest.ps");
+    myfile << "gsave\n"
+    << "0 0 moveto\n"
+    << _width << " 0 lineto\n"
+    << _width << " " << _height << " lineto\n"
+    << "0 " << _height << " lineto\n"
+    << "0 0 lineto\n"
+    << "stroke\n"
+    << "grestore";
+    myfile.close();
+}
+
+int Rectangle::getWidth(){
+    return _width;
+}
+
+int Rectangle::getHeight(){
+    return _height;
+}
+
+
