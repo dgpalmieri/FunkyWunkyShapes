5cef365e1a0125e0dee7ecd85025318ca56806b9
Dylan Palmieri <dgpalmieri@gmail.com>
2019-03-29 16:15:33 -0800

    Overload Spacer.intoPS()

--- Shapes/Spacer.cpp
+++ Shapes/Spacer.cpp
@@ -10,18 +10,33 @@ Spacer::Spacer(const double & width, const double & height)
 :_width(std::abs(width)), _height(std::abs(height)) {}
 
 //Same uncertainty that I have about rectangle the only difference is that the lines are white
+
 void Spacer::intoPS() {
-    std::ofstream myfile;
-    myfile.open("PostScriptTest.ps");
-    myfile << "gsave\n"
-    << "0 0 moveto\n"
-    << _width << " 0 lineto\n"
-    << _width << " " << _height << " lineto\n"
-    << "0 " << _height << " lineto\n"
-    << "0 0 lineto\n"
-    << "grestore";
-    myfile.close();
+    std::fstream myOut;
+    return this->intoPS(myOut, "PostScriptTest.ps");
 }
+
+void Spacer::intoPS(const std::string &fileName) {
+    std::fstream myOut;
+    return this->intoPS(myOut, fileName);
+}
+
+void Spacer::intoPS(std::fstream &fileStream) {
+    return this->intoPS(fileStream, "PostScriptTest.ps");
+}
+
+void Spacer::intoPS(std::fstream &fileStream, const std::string &fileName) {
+    fileStream.open(fileName);
+    fileStream << "gsave\n"
+               << "0 0 moveto\n"
+               << _width << " 0 lineto\n"
+               << _width << " " << _height << " lineto\n"
+               << "0 " << _height << " lineto\n"
+               << "0 0 lineto\n"
+               << "grestore";
+    fileStream.close();
+}
+
 
 double Spacer::getWidth(){
     return _width;
