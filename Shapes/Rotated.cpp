8fc1b1224c8d881ff0b69e72ddad4f4856a63e19
arparker5 <arparker5@alaska.edu>
2019-03-31 11:04:20 -0800

    bug fix on rotated

--- Shapes/Rotated.cpp
+++ Shapes/Rotated.cpp
@@ -9,15 +9,6 @@
 
 Rotated::Rotated(std::unique_ptr<Shape> shape, const int angle): _shape(std::move(shape)), _angle(angle){}
 
-void Rotated::intoPS(){
-    std::ofstream myfile;
-    myfile.open("PostScriptTest.ps", std::ios_base::app);
-    myfile << _shape->getCenter().first << " " <<  _shape->getCenter().second << " translate\n"
-            << _angle << " rotate\n"
-            << _shape->getCenter().first*(-1) << " " <<  _shape->getCenter().second*(-1) << " translate\n";
-    myfile.close();
-    _shape->intoPS();
-}
 double Rotated::getWidth(){
     if(_angle == 90 || _angle == 270){
         return _shape->getHeight();
@@ -32,6 +23,27 @@ double Rotated::getHeight(){
 }
 
 std::pair<double,double> Rotated::getCenter(){}
-void Rotated::intoPS(const std::string & fileName){}
-void Rotated::intoPS(std::fstream & fileStream){}
-void Rotated::intoPS(std::fstream & fileStream, const std::string & fileName){}
+
+void Rotated::intoPS() {
+    std::fstream myOut;
+    return this->intoPS(myOut, "PostScriptTest.ps");
+}
+
+void Rotated::intoPS(const std::string &fileName) {
+    std::fstream myOut;
+    return this->intoPS(myOut, fileName);
+}
+
+void Rotated::intoPS(std::fstream &fileStream) {
+    return this->intoPS(fileStream, "PostScriptTest.ps");
+}
+
+void Rotated::intoPS(std::fstream &fileStream, const std::string &fileName) {
+    std::ofstream myfile;
+    myfile.open("PostScriptTest.ps", std::ios_base::app);
+    myfile << _shape->getCenter().first << " " <<  _shape->getCenter().second << " translate\n"
+           << _angle << " rotate\n"
+           << _shape->getCenter().first*(-1) << " " <<  _shape->getCenter().second*(-1) << " translate\n";
+    myfile.close();
+    _shape->intoPS();
+}
