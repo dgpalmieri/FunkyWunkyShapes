8054eb6834c6a3d4d26d7eda8f818f48136e8bb9
Dylan Palmieri <dgpalmieri@gmail.com>
2019-04-15 23:42:33 -0800

    Refactor Layered to derive from Composite

--- Shapes/Layered.hpp
+++ Shapes/Layered.hpp
@@ -10,12 +10,12 @@
 #define Layered_hpp
 
 
-#include "Shape.h"
+#include "Composite.hpp"
 #include <string>
 #include <memory>
 #include <vector>
 
-class Layered : public Shape{
+class Layered : public Composite{
 public:
     explicit Layered(std::vector<std::unique_ptr<Shape>> & myShapes);
     void compositeType(std::fstream &, const std::string &) override;
